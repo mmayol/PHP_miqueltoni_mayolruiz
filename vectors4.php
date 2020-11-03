631e66ffd191f59e196a10a1ede2ea071025dc27
mmayol <72940561+mmayol@users.noreply.github.com>
2020-11-03 15:34:09 +0100

    Add files via upload

---
+++ vectors4.php
@@ -0,0 +1,13 @@
+<?php
+
+$key=array(
+    "January" => "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31.",
+    "Menu" => "Beef with mushroom sauce, spaghetti carbonara, Spanish omelette.",
+    "Alarm times" => "8, 12, 14, 16, 20",
+    "Product 123" => "PC, Mouse, Keyboard, i3core, 8GB RAM, 1TB",
+  );
+
+foreach ($key as $k => $string){
+  echo "$k: $string <br><br>";
+}
+?>
