73a96d8cec59dbf7499b410b6c6219103434b42a
mmayol <72940561+mmayol@users.noreply.github.com>
2020-11-12 20:30:33 +0100

    Delete motriu.php

--- motriu.php
+++
@@ -1,6 +0,0 @@
-<?php
-$month = array ("january","february","march","april","may","june","july","august","september","october","november","december");
-foreach ( $month as $value ) {
-echo "$value"," ";
-}
-?>
