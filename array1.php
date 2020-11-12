611f6c8c1fae996d563e9f7c6cda3fa2ca061502
mmayol <72940561+mmayol@users.noreply.github.com>
2020-11-12 20:39:25 +0100

    Delete array1.php

--- array1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$day = array ("monday", "tuesday", "wednesday", "thursday", "fryday","saturday","sunday");
-for ($d = 0; $d <= 6; $d+=1) {
-echo "Today is " . $day[$d]," the day $d of the week","<br>";
-}
-?>
