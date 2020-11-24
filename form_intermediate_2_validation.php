e49605bc2405e19842100a9346f0ac6389a23ac3
miquel toni <mmayol13167@alumnes.iesmanacor.cat>
2020-11-24 19:11:00 +0100

    form_intermediate_2_validation.php

---
+++ form_intermediate_2_validation.php
@@ -0,0 +1,20 @@
+<?php
+
+$name="Miquel Toni Mayol Ruiz";
+function fullNameSplit($name){
+return str_split ($name,6);
+}
+$sep=fullNameSplit($name);
+foreach ($sep as $frag){
+echo $frag, "<br>";}
+
+echo "<br>","<br>";
+
+$name2="Miquel Toni Mayol Ruiz";
+function fullNameSplit2($name2){
+return explode (" ",$name2);
+}
+$sep2=fullNameSplit2($name2);
+foreach ($sep2 as $frag2){
+echo $frag2, "<br>";}
+?>
