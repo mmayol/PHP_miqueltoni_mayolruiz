82ea9ec9463ad3e4cdb64f35eb95cf9cae1fd125
miquel toni <mmayol13167@alumnes.iesmanacor.cat>
2020-12-03 20:57:02 +0100

    Final_marking_task

---
+++ form_product_validation.php
@@ -0,0 +1,28 @@
+<?php
+
+function messageReplacement( $capitalisedName,$finalPrice){
+  $message="PRODUCTNAME final price is FINALPRICE €";
+  //use the php function str_ireplace that return a string
+  //example of use: $replacedString = str_ireplace("body", "full", "body combat");
+  //where $replacedString get the value "full combat"
+
+  return str_ireplace("FINALPRICE",$finalPrice,str_ireplace("PRODUCTNAME",$capitalisedName,$message));
+}
+//
+function discountCalculation($discount,$price){
+  //calculate the final amount after applying the discount and return it
+  // if the discount calculation is below 0 return false
+  $discountCalculation= $price - $discount;
+  if($discountCalculation <= 0)
+    return false;
+  else
+    return true;
+}
+
+function nameToCapitalLetters($name){
+  //use the php function strtoupper that return a string
+  //example of use: $transformedString = strtoupper("hello world")
+  //where $transformedString get the value "HELLO WORLD"
+    return strtoupper($name);
+}
+?>
